10c40af8a7015893d6a154be4c76446d501a1159
ericguizzo <eric.guizzo@hotmail.it>
2021-09-29 13:06:11 +0200

    cog-ified

--- predict.py
+++ predict.py
@@ -20,8 +20,11 @@ class Predictor(cog.Predictor):
     #@cog.input("input", type=Path, help="Audio file")
     def predict(self):
         """Compute prediction"""
-        tflib.init_tf()
-        _G, _D, Gs = pickle.load(open("pretrained_models/network-snapshot-020400.pkl", "rb"))
-        Gs.print_layers()
-        self.Gs = Gs
+        #tflib.init_tf()
+        #_G, _D, Gs = pickle.load(open("pretrained_models/network-snapshot-020400.pkl", "rb"))
+        #Gs.print_layers()
+        #self.Gs = Gs
         results_dir = "prova"
+        tflib.init_tf()
+        os.makedirs(result_dir, exist_ok=True)
+        draw_truncation_trick_figure(os.path.join(config.result_dir, 'truncation-trick.png'), load_Gs(model_place), w=128, h=128, seeds=[np.random.randint(0,100000),np.random.randint(0,100000), np.random.randint(0,100000), np.random.randint(0,100000), np.random.randint(0,12345), np.random.randint(0,12345),np.random.randint(0,12345)], psis=[1,0.9, 0.7, 0.5, 0, -0.5, -1], labels_exist = True)
